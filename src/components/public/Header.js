3ee5a905df87441d05975324efb7a235922f80af
breno <breno500as@gmail.com>
2017-04-17 17:33:15 -0300

    aaaa

--- src/components/public/Header.js
+++ src/components/public/Header.js
@@ -10,7 +10,12 @@ export default class Header extends Component {
             <header className="mdc-toolbar mdc-toolbar--fixed">
                 <div className="mdc-toolbar__row">
                     <section className="mdc-toolbar__section mdc-toolbar__section--align-start">
-                        <span className="mdc-toolbar__title">Anotaai</span>
+                       <Link to="/home"> <span className="mdc-toolbar__title">Anotaai</span></Link>
+                    </section>
+                    <section className="mdc-toolbar__section mdc-toolbar__section--align-end" role="toolbar">
+                       <Link to="/home" className="material-icons">file_download</Link>
+                       <Link to="/home"  className="material-icons">print</Link>
+                       <Link to="/home" className="material-icons">bookmark</Link>
                     </section>
                 </div>
             </header>);
