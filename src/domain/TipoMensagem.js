c30297066906ecc3deb833778adf80aa660002e6
breno <breno500as@gmail.com>
2017-05-04 08:58:09 -0300

    runtime error

--- src/domain/TipoMensagem.js
+++ src/domain/TipoMensagem.js
@@ -11,7 +11,7 @@ export class TipoMensagem extends DataType {
 
 }
 
-TipoMensagem.SUCCESS = new TipoMensagem(0, 'SUCCESS', 'green darken-2', ICON.DONE);
-TipoMensagem.INFO = new TipoMensagem(1, 'INFO', 'Indigo', ICON.INFO);
-TipoMensagem.WARNING = new TipoMensagem(2, 'WARNING', 'yellow darken-2', ICON.WARNING);
-TipoMensagem.ERROR = new TipoMensagem(3, 'ERROR', 'red darken-2', ICON.ERROR);
+//TipoMensagem.SUCCESS = new TipoMensagem(0, 'SUCCESS', 'green darken-2', ICON.DONE);
+//TipoMensagem.INFO = new TipoMensagem(1, 'INFO', 'Indigo', ICON.INFO);
+//TipoMensagem.WARNING = new TipoMensagem(2, 'WARNING', 'yellow darken-2', ICON.WARNING);
+//TipoMensagem.ERROR = new TipoMensagem(3, 'ERROR', 'red darken-2', ICON.ERROR);
