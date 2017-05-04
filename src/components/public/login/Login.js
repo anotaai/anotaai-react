5bdd9096021b6bce12681884812cbd6e2761ebf1
Gleidson Guimarães Moura <gleidson.gmoura@gmail.com>
2017-05-04 00:52:36 -0300

    mensagem

--- src/components/public/login/Login.js
+++ src/components/public/login/Login.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import MaskedInput from 'react-maskedinput'
 import ModalRenewPassword from './ModalRenewPassword'
 import UsuarioService from '../../../services/UserService'
-import { getObjectNewState , createInstance } from '../../../helpers/jsonHelper'
-import ShowMessage  from '../../../helpers/ShowMessage'
+import { getObjectNewState, createInstance } from '../../../helpers/jsonHelper'
+import ShowMessage from '../../../helpers/ShowMessage'
 import { TipoMensagem } from '../../../domain/TipoMensagem'
 
 export class RadioUser extends Component {
@@ -28,12 +28,12 @@ export class RadioUser extends Component {
     let field = null;
 
     if (this.state.tipoContato === 'telefone') {
-      field = <MaskedInput id='telefone'   onChange={this.handleInputChange.bind(this)} mask="(11) 11111-1111"   name="userLogin.usuario.telefone" required placeholder="Telefone" />
+      field = <MaskedInput id='telefone' onChange={this.handleInputChange.bind(this)} mask="(11) 11111-1111" name="userLogin.usuario.telefone" required placeholder="Telefone" />
     } else {
-      field = <div> 
-               <input id="email" className='validate' type='email' onChange={this.handleInputChange.bind(this)}   name="userLogin.usuario.email" required placeholder="Email" /> 
-               <label htmlFor="email" data-error="Email inválido" />
-             </div>
+      field = <div>
+        <input id="email" className='validate' type='email' onChange={this.handleInputChange.bind(this)} name="userLogin.usuario.email" required placeholder="Email" />
+        <label htmlFor="email" data-error="Email inválido" />
+      </div>
     }
 
     return (
@@ -59,7 +59,7 @@ export default class Login extends Component {
 
   constructor() {
     super();
-    this.state = { showModal: false  , userLogin:{usuario: {email:'',telefone:'',senha:''}, tipoAcesso: ''}};
+    this.state = { showModal: false, userLogin: { usuario: { email: '', telefone: '', senha: '' }, tipoAcesso: '' } };
   }
 
   showModal(e) {
@@ -71,29 +71,29 @@ export default class Login extends Component {
     this.setState({ showModal: false });
   }
 
-   handleInputChange(e) {
-     const newState = getObjectNewState(e.target.name, e.target.value, this.state);
-     this.setState(newState);
+  handleInputChange(e) {
+    const newState = getObjectNewState(e.target.name, e.target.value, this.state);
+    this.setState(newState);
   }
 
   login(e) {
     e.preventDefault();
-    this.context.store.dispatch(UsuarioService.login(this.state.userLogin,this.keepAlive.checked));
+    this.context.store.dispatch(UsuarioService.login(this.state.userLogin, this.keepAlive.checked));
   }
 
-   componentDidMount(){
-      this.context.store.subscribe(() => {
-        const errorMsg =  this.context.store.getState().auth.errorMsg;
-        if(errorMsg !== '') {
-           ShowMessage.show(errorMsg, TipoMensagem.ERROR);
-           const newState = createInstance(this.state);
-           newState.userLogin.usuario.senha = '';
-           this.setState(newState);
-        }
-      })
+  componentDidMount() {
+    this.context.store.subscribe(() => {
+      const errorMsg = this.context.store.getState().auth.errorMsg;
+      if (errorMsg !== '') {
+        ShowMessage.show(errorMsg, TipoMensagem.ERROR);
+        const newState = createInstance(this.state);
+        newState.userLogin.usuario.senha = '';
+        this.setState(newState);
+      }
+    })
   }
 
-  
+
   render() {
 
     return (
@@ -101,27 +101,27 @@ export default class Login extends Component {
         <center>
           <div className="section"></div>
           <h5 className="indigo-text">Login</h5>
-         
+
           <div className="container">
-            <div className="z-depth-1 row panel" style={{ display:'inline-block'}}>
+            <div className="z-depth-1 row panel" style={{ display: 'inline-block' }}>
               <form className="col s12" method="post" onSubmit={this.login.bind(this)}>
-                <RadioUser idEmail="idEmailLogin" idTelefone="idTelefoneLogin"   handleInputChange={this.handleInputChange.bind(this)} />
+                <RadioUser idEmail="idEmailLogin" idTelefone="idTelefoneLogin" handleInputChange={this.handleInputChange.bind(this)} />
                 <div className='row'>
                   <div className='input-field col s12'>
-                    <input id="senha"  type='password' name='userLogin.usuario.senha' value={this.state.userLogin.usuario.senha}  placeholder="Senha"   required onChange={this.handleInputChange.bind(this)} />
+                    <input id="senha" type='password' name='userLogin.usuario.senha' value={this.state.userLogin.usuario.senha} placeholder="Senha" required onChange={this.handleInputChange.bind(this)} />
                   </div>
                 </div>
-                <div className='row'> 
-                  <div className='col s7 left-align'>  
-                     <input type="checkbox" id="manterConectado" ref={(input) => this.keepAlive = input} />
-                     <label  htmlFor="manterConectado">Manter conectado</label>
+                <div className='row'>
+                  <div className='col s7 left-align'>
+                    <input type="checkbox" id="manterConectado" ref={(input) => this.keepAlive = input} />
+                    <label htmlFor="manterConectado">Manter conectado</label>
                   </div>
-                  <label className='col s5 right-align' style={{marginTop:'3px'}}>
+                  <label className='col s5 right-align' style={{ marginTop: '3px' }}>
                     <a href="#" onClick={this.showModal.bind(this)} className="pink-text" >Esqueceu a senha?</a>
                   </label>
-                  <div className='col s12' />  
+                  <div className='col s12' />
                 </div>
-                <br/>
+                <br />
                 <center>
                   <div className='row'>
                     <button type='submit' className='col s12 btn btn-large info'>Login</button>
@@ -130,8 +130,8 @@ export default class Login extends Component {
               </form>
             </div>
           </div>
-        </center> 
-         <ModalRenewPassword showModal={this.state.showModal} callbackHideModal={this.hideModal.bind(this)} />
+        </center>
+        <ModalRenewPassword showModal={this.state.showModal} callbackHideModal={this.hideModal.bind(this)} />
       </main>
 
     );
@@ -140,5 +140,5 @@ export default class Login extends Component {
 }
 
 Login.contextTypes = {
-  store : React.PropTypes.object.isRequired
+  store: React.PropTypes.object.isRequired
 }
