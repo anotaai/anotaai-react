790a90080a8aaef5adf1f15f048140f31075ee60
breno500as <breno500as@gmail.com>
2017-05-04 23:27:34 -0300

    profile

--- src/components/private/SideMenu.js
+++ src/components/private/SideMenu.js
@@ -2,33 +2,41 @@ import React, { Component } from 'react'
 import MenuService from '../../services/menu/MenuService'
 import { Link } from 'react-router'
 import blank_avatar from "../../img/blank_avatar.png"
+import $ from 'jquery'
 
 
 class Profile extends Component {
 
-    render() {
-         
-         return (
-           <li> 
-             <div className="userView userDetais">
-               <div className="row">
-                 <div className="col col s8 m8 l8">
-                   <img src={blank_avatar} alt="Avatar" className="circle responsive-img profile-image" />
-                 </div>
-                 <div className="row">
-                   <a className="btn-flat dropdown-button waves-effect waves-light white-text profile-btn" href="#" data-activates='dropdown1'>Grande Olinda!<i className="material-icons right">arrow_drop_down</i></a>
-                   <ul id="dropdown1" className="dropdown-content">
-                     <li><a href="#!">Settings<i className="material-icons">settings</i></a></li>
-                     <li className="divider"></li>
-                     <li><a href="#!">Logout<i className="material-icons">power_settings_new</i></a></li>
-                   </ul>
-                 </div>
-                </div>
-              </div> 
-           </li>
-         );
-        
-    }
+
+  componentDidMount() {
+    $('#profile').dropdown({});
+  }
+
+  render() {
+
+    return (
+      <li>
+        <div className="userView profile-details">
+          <div className="row">
+            <div className="col col s4 m4 l4">
+              <img src={blank_avatar} alt="Avatar" className="circle responsive-img profile-image" />
+            </div>
+          </div>
+          <div className="row">
+             <div className="col col s8 m8 l8">
+              <a id="profile" className="btn-flat dropdown-button waves-effect waves-light white-text profile-btn"  href="#" data-activates='dropdown1'>Anota ai<i className="material-icons right">arrow_drop_down</i></a>
+              <ul id="dropdown1" className="dropdown-content">
+                <li><a href="#!">Settings<i className="material-icons">settings</i></a></li>
+                <li className="divider"></li>
+                <li><a href="#!">Logout<i className="material-icons">power_settings_new</i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </li>
+    );
+
+  }
 
 }
 
@@ -36,16 +44,16 @@ export default class SideMenu extends Component {
 
   constructor() {
     super();
-    this.state = {listMenu:[]};
+    this.state = { listMenu: [] };
   }
 
   componentDidMount() {
-       MenuService.getMenu().then(response => {
-           this.setState({listMenu:response});
-       }).catch(error => {
-           alert(error);
-       });
-  } 
+    MenuService.getMenu().then(response => {
+      this.setState({ listMenu: response });
+    }).catch(error => {
+      alert(error);
+    });
+  }
 
 
   render() {
@@ -53,11 +61,11 @@ export default class SideMenu extends Component {
     return (
       <ul className="side-nav fixed">
         <Profile />
-        {this.state.listMenu.map(itensMenu => 
-          ( <div key={itensMenu.action}> 
-             <li><Link to="/home"><i className="material-icons">contacts</i>{itensMenu.descricao}</Link></li>
-             <li><div className="divider"></div></li>
-            </div>
+        {this.state.listMenu.map(itensMenu =>
+          (<div key={itensMenu.action}>
+            <li><Link to="/home"><i className="material-icons">contacts</i>{itensMenu.descricao}</Link></li>
+            <li><div className="divider"></div></li>
+          </div>
           )
         )}
       </ul>
