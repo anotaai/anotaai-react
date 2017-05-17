210de6f89c4ee677a8bdb20b119fd25dd0face5a
Gleidson Guimarães Moura <gleidson.gmoura@gmail.com>
2017-05-16 23:53:17 -0300

    foto perfil

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,13 +1,13 @@
-import { AUTH_USER, UNAUTH_USER} from '../actions/authActionCreator';
+import { AUTH_USER, UNAUTH_USER } from '../actions/authActionCreator';
 
-const INITIAL_STATE = { loginState: null}
+const INITIAL_STATE = { loginState: null }
 
-export default function (state = INITIAL_STATE, action) {  
-  switch(action.type) {
+export default function (state = INITIAL_STATE, action) {
+  switch (action.type) {
     case AUTH_USER:
-      return { ...state, loginState: action.loginState};
+      return { ...state, loginState: action.loginState };
     case UNAUTH_USER:
-      return { ...state, loginState: null};
+      return { ...state, loginState: null };
     default:
       return state;
   }
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -10,120 +10,118 @@ import AuthenticationService from './app/AuthenticationService'
 export default class UserService {
 
 
-    static  getUser(activationCode) {
+    static getUser(activationCode) {
 
-     return fetch(`${process.env.REACT_APP_URL_BACKEND}/rest/usuarios/recuperarUsuarioAlteracaoSenha`,{
-        method: 'POST',
-        headers: {'Content-type' : 'application/json'},
-        body: activationCode})
-        .then(response => {
-            return response.json();
-        }).catch(error => {
-            throw Error(error);
+        return fetch(`${process.env.REACT_APP_URL_BACKEND}/rest/usuarios/recuperarUsuarioAlteracaoSenha`, {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: activationCode
         })
-         
+            .then(response => {
+                return response.json();
+            }).catch(error => {
+                throw Error(error);
+            })
     }
 
     static changePassword(user) {
-           
+
         return fetch(`${process.env.REACT_APP_URL_BACKEND}/rest/usuarios/alterarSenha`, {
             method: 'POST',
-            headers: {'Content-type':'application/json'},
-            body: JSON.stringify(user) 
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(user)
         }).then(response => {
-           return response.json();
+            return response.json();
         }).catch(error => {
-           throw Error(error);
+            throw Error(error);
         });
 
     }
 
     static askNewPassword(user) {
+        const telefone = user.telefone;
+        const newUserInstance = createInstance(user);
+        newUserInstance.telefone = buildPhone(telefone);
 
-
-         const telefone = user.telefone; 
-         const newUserInstance = createInstance(user);
-         newUserInstance.telefone = buildPhone(telefone);
-
-         return fetch(`${process.env.REACT_APP_URL_BACKEND}/rest/usuarios/solicitarMensagemAlteracaoSenha`,
-         { method: 'POST', 
-           body:  JSON.stringify(newUserInstance),
-           headers: {'Content-type': 'application/json'}})
-          .then(response => {
-             return response.json();
-          }).catch(error => {
-             throw Error(error);
-          });
-
+        return fetch(`${process.env.REACT_APP_URL_BACKEND}/rest/usuarios/solicitarMensagemAlteracaoSenha`,
+            {
+                method: 'POST',
+                body: JSON.stringify(newUserInstance),
+                headers: { 'Content-type': 'application/json' }
+            })
+            .then(response => {
+                return response.json();
+            }).catch(error => {
+                throw Error(error);
+            });
     }
 
 
     static activation(code) {
-         
-         return fetch(`${process.env.REACT_APP_URL_BACKEND}/rest/usuarios/activation`,{
-              headers: {'Content-type' : 'application/json'},
-              body: code,
-              method: 'POST'
-         }).then(response => {
-             return response.json();
-         }).catch(error => {
-             throw Error(error);
-         });
+        return fetch(`${process.env.REACT_APP_URL_BACKEND}/rest/usuarios/activation`, {
+            headers: { 'Content-type': 'application/json' },
+            body: code,
+            method: 'POST'
+        }).then(response => {
+            return response.json();
+        }).catch(error => {
+            throw Error(error);
+        });
     }
 
     static save(usuario, telefoneStr) {
-        
+
         const newUserInstance = createInstance(usuario);
         newUserInstance.telefone = buildPhone(telefoneStr);
 
         return fetch(`${process.env.REACT_APP_URL_BACKEND}/rest/usuarios`, {
             method: 'POST',
             body: JSON.stringify(newUserInstance),
-            headers: {'Content-type': 'application/json'}
-            }).then(response => {
-              return response.json();
-            }).catch(error => {
-              throw Error(error);
-            });
+            headers: { 'Content-type': 'application/json' }
+        }).then(response => {
+            return response.json();
+        }).catch(error => {
+            throw Error(error);
+        });
 
     }
 
     static logout(login) {
-    
-       return  new Promise((resolve,reject) => {
- 
-           return fetch(`${process.env.REACT_APP_URL_BACKEND}/rest/usuarios/logout`,{
+
+        return new Promise((resolve, reject) => {
+
+            return fetch(`${process.env.REACT_APP_URL_BACKEND}/rest/usuarios/logout`, {
                 method: 'POST',
                 body: JSON.stringify(login),
-                headers: { 'Content-type':'application/json'}
+                headers: { 'Content-type': 'application/json' }
             }).then(response => {
                 resolve(response.json());
             }).catch(error => {
                 reject(error);
             })
-            
-       });
+
+        });
     }
 
-    static dispatchLogout()  {
+    static dispatchLogout() {
 
         return dispatch => {
             dispatch(unauthUser());
             browserHistory.push(URL.LOGIN);
         }
-    } 
+    }
 
-    static login(usuarioLogin,keepAlive) {
+    static login(usuarioLogin, keepAlive) {
 
         return new Promise((resolve, reject) => {
 
             const newUserLoginInstance = createInstance(usuarioLogin);
             const newUserInstance = createInstance(usuarioLogin.usuario);
-          
-            const tipoAcesso = usuarioLogin.usuario.email !== '' ? 'EMAIL'  : 'TELEFONE';
+
+            const tipoAcesso = usuarioLogin.usuario.email !== '' ? 'EMAIL' : 'TELEFONE';
             const telefone = usuarioLogin.usuario.telefone;
             const senha = usuarioLogin.usuario.senha;
-            
+
             newUserLoginInstance.tipoAcesso = tipoAcesso;
             newUserInstance.telefone = buildPhone(telefone);
             newUserInstance.senha = Base64Service.encode(senha);
@@ -132,7 +130,7 @@ export default class UserService {
             return fetch(`${process.env.REACT_APP_URL_BACKEND}/rest/usuarios/login`, {
                 method: 'POST',
                 body: JSON.stringify(newUserLoginInstance),
-                headers: {'Content-type': 'application/json'}
+                headers: { 'Content-type': 'application/json' }
             }).then(response => {
                 resolve(response.json());
             }).catch(error => {
@@ -141,14 +139,27 @@ export default class UserService {
         });
 
     }
-   
+
 
     static dispatchLogin(response) {
         return dispatch => {
-           const loginState = AuthenticationService.setCredentials(response);
-           dispatch(authUser(loginState));
-           browserHistory.push(URL.HOME);
+            const loginState = AuthenticationService.setCredentials(response);
+            dispatch(authUser(loginState));
+            browserHistory.push(URL.HOME);
         }
+    }
+
+    static loadProfileImage() {
+        return new Promise((resolve, reject) => {
+            return fetch(`${process.env.REACT_APP_URL_BACKEND}/rest/usuarios/profilePhoto`, {
+                method: 'GET',
+                headers: { 'Content-type': 'application/json' }
+            }).then(response => {
+                resolve(response.json());
+            }).catch(error => {
+                reject(error);
+            })
+        });
     }
 
 }
