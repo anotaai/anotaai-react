ed5525a1838565aeb5320072dc25da1ec05ae5c0
Gleidson Guimarães Moura <gleidson.gmoura@gmail.com>
2017-05-20 18:13:29 -0300

    'icon'

---
+++ src/domain/Icon.js
@@ -0,0 +1,16 @@
+import { DataType } from './DataType'
+
+export class Icon extends DataType {
+    
+
+    constructor(ordinal, type, className) {
+        super(ordinal, type);
+        this.className = className;
+    }
+
+}
+
+Icon.DONE = new Icon(0, 'DONE', 'done');
+Icon.INFO = new Icon(1, 'INFO', 'info');
+Icon.WARNING = new Icon(2, 'WARNING', 'warning');
+Icon.ERROR = new Icon(3, 'ERROR', 'error');
