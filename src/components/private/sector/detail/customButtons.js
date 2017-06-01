87371975fc5aa6d08ede1303cfdccdc97eb2c6b4
breno <breno500as@gmail.com>
2017-06-01 08:57:13 -0300

    responsive

--- src/components/private/sector/detail/customButtons.js
+++ src/components/private/sector/detail/customButtons.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React   from 'react'
 import { push } from '../../../App'
 import { URL } from '../../../../helpers/constants'
 
 export function CustomButtons(props) {
-    return 
-    (<span>
+    return  ( 
+      <span>
         <button className="btn waves-effect buttons-space INFO" onClick={push.bind(this, URL.GROUP_PRODUCT)}>
             Grupo Produto <i className="material-icons right">apps</i>
         </button>
@@ -16,8 +16,8 @@ export function CustomButtons(props) {
 
 export function CustomResponsiveButtons(props) {
    
-   return
-    (<span>
+   return (
+     <span>
         <li>
             <div className="row">
                 <div className="col col s2 offset-s8">
