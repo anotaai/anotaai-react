e1bfbdd4f741cdf307b871ebe25c840e6dbdaa78
breno <breno500as@gmail.com>
2017-07-07 16:16:50 -0300

    menu reducer

--- src/reducers/menuReducer.js
+++ src/reducers/menuReducer.js
@@ -1,39 +1,39 @@
 import { UPDATE_MENU, TOGGLE_MENU } from '../actions/menuActionCreator'
 import { createInstance } from '../helpers/jsonHelper'
 
- const FULL_CLASS_CONTENT = 'col s12 l12', NORMAL_CLASS_CONTENT = 'col s12 l10' , 
-HIDE_CLASS_MENU = 'hide-menu' ,  NORMAL_CLASS_MENU = 'col s2 l2 show-menu' , 
-INITIAL_STATE = {listMenu:[], showMenu: true , classMenu: NORMAL_CLASS_MENU , classContent: NORMAL_CLASS_CONTENT} 
+const FULL_CLASS_CONTENT = 'col s12 l12', NORMAL_CLASS_CONTENT = 'col s12 l10',
+    HIDE_CLASS_MENU = 'hide-menu', NORMAL_CLASS_MENU = 'col s2 l2 show-menu',
+    INITIAL_STATE = { listMenu: [], showMenu: true, classMenu: NORMAL_CLASS_MENU, classContent: NORMAL_CLASS_CONTENT }
 
 
-export default function (state = INITIAL_STATE,action) {
- 
-  switch (action.type) {
-       
-       case UPDATE_MENU: {
-           const newState = createInstance(state);
-           newState.listMenu = action.itens;
-           return newState
-       }
+export default function (state = INITIAL_STATE, action) {
+
+    switch (action.type) {
+
+        case UPDATE_MENU: {
+            const newState = createInstance(state);
+            newState.listMenu = action.itens;
+            return newState
+        }
 
         case TOGGLE_MENU: {
-           const newState = createInstance(state);
-           
-           if(newState.showMenu) {
-              newState.showMenu = false;
-              newState.classMenu =  HIDE_CLASS_MENU;
-              newState.classContent =  FULL_CLASS_CONTENT;    
-           } else {
-              newState.showMenu = true;
-              newState.classMenu =  NORMAL_CLASS_MENU;
-              newState.classContent =  NORMAL_CLASS_CONTENT;
-           }
+            const newState = createInstance(state);
 
-           return newState;
-       }
+            if (newState.showMenu) {
+                newState.showMenu = false;
+                newState.classMenu = HIDE_CLASS_MENU;
+                newState.classContent = FULL_CLASS_CONTENT;
+            } else {
+                newState.showMenu = true;
+                newState.classMenu = NORMAL_CLASS_MENU;
+                newState.classContent = NORMAL_CLASS_CONTENT;
+            }
 
-       default:
-        return state;
-  }
+            return newState;
+        }
+
+        default:
+            return state;
+    }
 
 }
